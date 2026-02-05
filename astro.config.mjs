e713e90212f1bcfc8d28fca7a468489249d37132
SpreadSheets600 <sohammaity239@gmail.com>
2026-02-05 14:50:11 +0000

    fix: css first time to byte loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   output: "server",
-  integrations: [mdx()],
+  integrations: [mdx(), tailwind()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
